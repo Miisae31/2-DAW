090b884e1866bbd34368d04201da627c4a80c21d
Miisae31 <153545996+Miisae31@users.noreply.github.com>
2024-09-24 14:31:43 +0200

    Add files via upload

---
+++ DWCS/1º Trimestre PHP/UD2 - 1 Comezando con PHP (2024-2025)/Ejercicio_1/index.php
@@ -0,0 +1,16 @@
+<?php
+$a = "HOLA MUNDO!";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    <h1><?php echo $a; ?></h1>
+  
+    
+</body>
+</html>
